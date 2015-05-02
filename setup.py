e3ce2d5ae123be1e1fe16384e4fd220da3f9e7b8
Sysel <marian.sysel.hornak@gmail.com>
2015-05-02 22:27:00 +0200

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="gaminator",
+    version="0.0.0",
+    author=u"Marián Horňák",
+    author_email="marian.sysel.hornak@gmail.com",
+    description=(""),
+    license="MIT",
+    keywords="",
+    url="https://github.com/syslo/gaminator",
+    packages=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+    ],
+)
