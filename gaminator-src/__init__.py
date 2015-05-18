a83424125e371d48149305e51a6f2dd875b59fdf
Sysel <marian.sysel.hornak@gmail.com>
2015-05-18 20:26:32 +0200

    Constants loading at global scope

--- gaminator-src/__init__.py
+++ gaminator-src/__init__.py
@@ -19,5 +19,7 @@ for name in dir(_pygame):
     if name.startswith("K_"):
         CONSTANTS[name] = getattr(_pygame, name)
 
+import sys as _sys
+_module = _sys.modules[__name__]
 for key, value in CONSTANTS.items():
-    setattr(PTI__game, key, value)
+    setattr(_module, key, value)
