62d0e4b8830bd70fc314c003f9bf692b42e85b5a
Sysel <marian.sysel.hornak@gmail.com>
2015-05-19 19:42:13 +0200

    PictureThing not resizable

---
+++ gaminator-src/exceptions.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+class PTI_exception__AssignmentUnsupported(Exception):
+    pass
--- gaminator-src/picture_thing.py
+++ gaminator-src/picture_thing.py
@@ -4,6 +4,7 @@
 from .thing import PTI__Thing
 from .canvas import PTI__Canvas
 from .picture import PTI__Picture
+from .exceptions import PTI_exception__AssignmentUnsupported
 
 
 class PTI__PictureThing(PTI__Thing):
@@ -13,19 +14,41 @@ class PTI__PictureThing(PTI__Thing):
         self.PTI__picture = Picture(20, 20)
 
     @property
+    def PTI__width(self):
+        return self._width
+
+    @PTI__width.setter
+    def PTI__width(self, PTI__value):
+        raise PTI_exception__AssignmentUnsupported()
+
+    @property
+    def PTI__height(self):
+        return self._height
+
+    @PTI__height.setter
+    def PTI__height(self, PTI__value):
+        raise PTI_exception__AssignmentUnsupported()
+
+    @property
     def PTI__picture(self):
-        return self._back_picture
+        return self._picture
 
     @PTI__picture.setter
     def PTI__picture(self, PTI__picture):
-        self._back_picture = PTI__picture
+        self._picture = PTI__picture
         self._canvas = PTI__Canvas(self._picture)
-        self.PTI__width = PTI__picture.width
-        self.PTI__height = PTI__picture.height
+        self._width = PTI__picture.width
+        self._height = PTI__picture.height
 
     @property
     def PTI__canvas(self):
         return self._canvas
 
+    def _repaint(self, canvas):
+        canvas.PTI_draw__picture(
+            self._picture,
+            (self.PTI_thing__border_left, self.PTI_thing__border_up),
+        )
+
     def PTI_thing__paint(self, PTI__canvas):
-        PTI__canvas.PTI__picture(self._back_picture, (0, 0))
+        pass
